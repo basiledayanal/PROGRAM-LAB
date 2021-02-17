d89b635bf0d7fdca10a95ea412e085fbbe67ba08
basiledayanal <76986813+basiledayanal@users.noreply.github.com>
2021-02-17 21:02:30 +0530

    Add files via upload

---
+++ CO2 pg 6.py
@@ -0,0 +1,10 @@
+test_str=str(input("Enter the string : ")) 
+freq = {} 
+  
+for i in test_str: 
+    if i in freq: 
+        freq[i] += 1
+    else: 
+        freq[i] = 1
+  
+print ("Count of all characters :\n "+  str(freq))
