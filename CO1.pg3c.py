506289e8de8c9c2e5aef94bb75265744136b89d4
basiledayanal <76986813+basiledayanal@users.noreply.github.com>
2021-03-04 21:47:24 +0530

    Update CO1.pg3c.py

--- CO1.pg3c.py
+++ CO1.pg3c.py
@@ -1,7 +1,4 @@
-w=input("Enter a sentence:")
-c=len(w)
-s=1
-for i in range(1,c):
-    if w[i]==" ":
-        s+=1
-print("Number of words are",s)
+vowels = "aeiuoAEIOU"
+word='Programming'
+    
+print([letter for letter in word if letter in vowels])
